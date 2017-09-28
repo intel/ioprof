d7fd67875a87033152e78a121851e68e90586300
bjdonie <benjamin.j.donie@intel.com>
2017-09-28 14:24:00 -0700

    Update header

--- ioprof.pl
+++ ioprof.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
-# I/O Profiler
-# Copyright (c) 2014, Intel Corporation.
+# I/O Profiler for Linux 
+# Copyright (c) 2017, Intel Corporation.
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms and conditions of the GNU General Public License,
